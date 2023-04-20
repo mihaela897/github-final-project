a4f6e52c5f4f65f61f7c58b7806926453963e6c6
mihaela897 <74004104+mihaela897@users.noreply.github.com>
2023-04-20 18:45:27 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <mihaela897>
+   # mihaela897
    # Input:
    # p, principal amount
    # t, time period in years
